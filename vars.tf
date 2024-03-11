3085a6e3208dc7f352bafaed9a9babb1e95aaf87
GUILHERME ENDRIGO <83622960+Guilherme-Endrigo@users.noreply.github.com>
2024-03-11 18:45:41 -0300

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -3,3 +3,23 @@ variable "regionDefault" {
   description = "aws region"
 }
 
+variable "projectName" {
+  default     = "foodieflow"
+  description = "project name"
+}
+
+variable "POSTGRES_USER" {
+  #default     = "postgres"
+  description = "The PostgreSQL user"
+  sensitive   = true
+}
+
+variable "POSTGRES_PASSWORD" {
+  description = "The PostgreSQL password"
+  sensitive   = true
+}
+
+variable "POSTGRES_DB" {
+  description = "The PostgreSQL database"
+  sensitive   = true
+}
